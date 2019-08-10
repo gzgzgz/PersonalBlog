0b115d24e67b74d53aa6fdb1e454c5e13474ad1f
guo <shark>
2019-08-09 20:12:54 -0400

    switch to PostgreSQL

--- Models/BlogDBContext.cs
+++ Models/BlogDBContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace Projects.Models
 {
@@ -11,7 +12,8 @@ namespace Projects.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source=/app/BlogDB.db");
+            // optionsBuilder.UseSqlite($"Data Source=BlogDB.db");
+            optionsBuilder.UseNpgsql($"Host=ec2-107-22-211-248.compute-1.amazonaws.com;Database=d7np0icrgf0e5p;Username=qbnruynckzauih;Password=54f888eb568392e1802867ddc0a25cf70927048cb05e97fbcb106cab8f3d8d1f");
         }
 
         public DbSet<BlogsEntityModel> Blogs {get; set;}
