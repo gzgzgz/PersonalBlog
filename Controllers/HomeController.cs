8b5592bd7779da6ac6c78928f02caccd5c3bea9a
guo <shark>
2019-08-09 20:55:46 -0400

    change again

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -53,7 +53,8 @@ namespace Projects.Controllers
             var db = new BlogDBContext(new DbContextOptions<BlogDBContext>());
             var selectedPost = db.Blogs.Where(x => x.BlogID == id).FirstOrDefault();
             db.Blogs.Remove(selectedPost);
-            db.SaveChangesAsync();
+            // db.SaveChangesAsync();
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -68,7 +69,8 @@ namespace Projects.Controllers
                 articleInEdit.Permission = editPost.Permission;
                 articleInEdit.Title = editPost.Title;
             }
-            db.SaveChangesAsync();
+            // db.SaveChangesAsync();
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -90,7 +92,8 @@ namespace Projects.Controllers
                 BlogHtml = newPost.BlogHtml,
                 Permission = newPost.Permission
             });
-            db.SaveChangesAsync();
+            // db.SaveChangesAsync();
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
